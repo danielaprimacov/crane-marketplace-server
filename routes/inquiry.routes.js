c5e96426d139eb72d5571fe452cf58225aa5763b
Daniela <daniela.primacov@yahoo.com>
2025-07-08 15:19:27 +0200

    Fixing the populate crane for inquiries

--- routes/inquiry.routes.js
+++ routes/inquiry.routes.js
@@ -45,10 +45,12 @@ router.get("/inquiries", isAuthenticated, async (req, res, next) => {
       return res.status(403).json("Forbidden: admin only");
     }
 
-    const allInquiries = await Inquiry.find({}).populate({
-      path: "crane",
-      select: "title",
-    });
+    const allInquiries = await Inquiry.find({})
+      .populate({
+        path: "crane",
+        select: "producer seriesCode variantRevision",
+      })
+      .exec();
     res.json(allInquiries);
   } catch (error) {
     next(error);
